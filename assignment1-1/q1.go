d8c419a545098ed9ce95a678e5c8ce34256d32bc
HamzaMumtaz721 <i232635@lhr.nu.edu.pk>
2026-04-29 14:47:00 +0500

    Completed assignment 1-1: topWords and parallel sum

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -6,15 +6,25 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-)
+        )
 
+// Find the top K most common words in a text document.
+// 	path: location of the document
+//	numWords: number of words to return (i.e. k)
+//	charThreshold: character threshold for whether a token qualifies as a word,
+//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+// Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
+// A word comprises alphanumeric characters only. All punctuation and other characters
+// are removed, e.g. "don't" becomes "dont".
+// You should use `checkError` to handle potential errors.
 func topWords(path string, numWords int, charThreshold int) []WordCount {
+	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
+	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
 	data, err := ioutil.ReadFile(path)
 	checkError(err)
 
 	text := strings.ToLower(string(data))
 
-	// Remove non-alphanumeric characters completely (don't replace with space)
 	re := regexp.MustCompile("[^0-9a-zA-Z\\s]+")
 	text = re.ReplaceAllString(text, "")
 
@@ -24,38 +34,43 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
 	for _, w := range tokens {
 		if len(w) >= charThreshold {
 			freq[w]++
-		}
-	}
+                }
+        }
 
 	var wordCounts []WordCount
 	for w, c := range freq {
-		wordCounts = append(wordCounts, WordCount{w, c})
-	}
+		wordCounts = append(wordCounts,
+			WordCount{w, c})
+                }
 
 	sortWordCounts(wordCounts)
 
 	if numWords > len(wordCounts) {
 		numWords = len(wordCounts)
-	}
+                        }
 	return wordCounts[:numWords]
-}
+        }
 
+// A struct that represents how many times a word is observed in a document
 type WordCount struct {
 	Word  string
 	Count int
-}
+        }
 
 func (wc WordCount) String() string {
 	return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
-}
+                        }
 
+// Helper function to sort a list of word counts in place.
+// This sorts by the count in decreasing order, breaking ties using the word.
+// DO NOT MODIFY THIS FUNCTION!
 func sortWordCounts(wordCounts []WordCount) {
 	sort.Slice(wordCounts, func(i, j int) bool {
 		wc1 := wordCounts[i]
 		wc2 := wordCounts[j]
 		if wc1.Count == wc2.Count {
 			return wc1.Word < wc2.Word
-		}
+                        }
 		return wc1.Count > wc2.Count
-	})
-}
+                })
+        }
--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -5,60 +5,65 @@ import (
 	"io"
 	"os"
 	"strconv"
-)
+        )
 
-// sumWorker receives numbers from nums channel, sums them, sends total to out
+// Sum numbers from channel `nums` and output sum to `out`.
+// You should only output to `out` once.
+// Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
+	// HINT: use for loop over `nums`
 	sum := 0
 	for n := range nums {
 		sum += n
-	}
+                }
 	out <- sum
-}
+        }
 
-// sum reads integers from fileName and sums using num goroutines
+// Read integers from the file `fileName` and return sum of all values.
+// This function must launch `num` go routines running
+// `sumWorker` to find the sum of the values concurrently.
+// You should use `checkError` to handle potential errors.
+// Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// Open file
+	// HINT: use `readInts` and `sumWorkers`
+	// HINT: used buffered channels for splitting numbers between workers
 	file, err := os.Open(fileName)
 	checkError(err)
 	defer file.Close()
 
-	// Read all integers from file
 	nums, err := readInts(file)
 	checkError(err)
 
-	// Create one buffered channel per worker
 	workerChans := make([]chan int, num)
 	for i := 0; i < num; i++ {
 		workerChans[i] = make(chan int, len(nums))
-	}
+                }
 
-	// Channel to collect partial sums from workers
 	out := make(chan int, num)
 
-	// Launch goroutines
 	for i := 0; i < num; i++ {
 		go sumWorker(workerChans[i], out)
-	}
+                }
 
-	// Distribute numbers across workers round-robin
 	for i, n := range nums {
 		workerChans[i%num] <- n
-	}
+                }
 
-	// Close worker channels so sumWorker for-range loops finish
 	for i := 0; i < num; i++ {
 		close(workerChans[i])
-	}
+                        }
 
-	// Collect all partial sums and add them up
 	total := 0
 	for i := 0; i < num; i++ {
 		total += <-out
-	}
+                }
 	return total
-}
+        }
 
+// Read a list of integers separated by whitespace from `r`.
+// Return the integers successfully read with no error, or
+// an empty slice of integers and the error that occurred.
+// Do NOT modify this function.
 func readInts(r io.Reader) ([]int, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanWords)
@@ -67,8 +72,8 @@ func readInts(r io.Reader) ([]int, error) {
 		val, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			return elems, err
-		}
+                        }
 		elems = append(elems, val)
-	}
+                }
 	return elems, nil
-}
+        }
